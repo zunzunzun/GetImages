d4d33643bc103cf28976fa83a4eb70465d49555e
zun <zun8ang@gmail.com>
2019-04-25 17:14:26 +0900

    이미지 list로 초기 구현

---
+++ GetImages/View/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  GetImages
+//
+//  Created by zun on 25/04/2019.
+//  Copyright © 2019 zun. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
